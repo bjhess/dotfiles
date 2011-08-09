04640788be39e0c79b3c88e42eec36c4ac75845c
Barry Hess <barry@bjhess.com>
2011-08-09 15:30:46 -0500

    Update watchr script for new 0.7 standards.

--- watchr/rails.rb
+++ watchr/rails.rb
@@ -73,25 +73,14 @@ watch('spec/.*/.*_spec\.rb') { |m| run_test_file(m[0]) }
 watch('app/.*/.*\.rb') { |m| related_test_files(m[0]).map {|tf| run_test_file(tf) } }
 watch('features/.*/.*\.feature') { run_all_features }
 
+# --------------------------------------------------
+# Signal Handling
+# --------------------------------------------------
 # Ctrl-\
-Signal.trap 'QUIT' do
-  puts " --- Running all tests and specs ---\n\n"
+Signal.trap('QUIT') do
+  puts " --- Running all tests ---\n\n"
   run_all_tests
-  run_all_specs
 end
-
-@interrupted = false
 
 # Ctrl-C
-Signal.trap 'INT' do
-  if @interrupted then
-    @wants_to_quit = true
-    abort("\n")
-  else
-    puts "Interrupt a second time to quit"
-    @interrupted = true
-    Kernel.sleep 1.5
-    # raise Interrupt, nil # let the run loop catch it
-    run_suite
-  end
-end
+Signal.trap('INT') { abort("\n") }
